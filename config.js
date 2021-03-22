92cc75fa2c0bc6d59e22266c8206982f99dba2d9
D.S <larastar721@gmail.com>
2021-03-22 21:02:11 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ const DB_USERNAME = process.env.DB_USERNAME;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 
 module.exports = {
-    jwtSecret: JWTSECRET,
+    jwtSecret: JWTSECRET||'123456',
     mongodburi: ''
 };
